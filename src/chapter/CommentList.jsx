09ef44715dc16db90ed9dae5dd39ef08f7b15885
sanmarkuh <sanmarkuh@naver.com>
2022-06-09 17:16:34 +0900

    2022/06/09_1

---
+++ src/chapter/CommentList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Comment from "./Comment";
+
+var comment = [
+    {name: "김희우1", comment: "안녕하세요, 김희우1입니다."},
+    {name: "김희우2", comment: "안녕하세요, 김희우2입니다."},
+    {name: "김희우3", comment: "안녕하세요, 김희우3입니다."}
+]
+
+function CommentList(props) {
+    return (
+        <div>
+            {comment.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment}></Comment>
+                );
+            })};
+        </div>
+    );
+}
+
+export default CommentList;
